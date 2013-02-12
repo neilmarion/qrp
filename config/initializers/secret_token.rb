876a327a68051af7106032c22b2fae7087d9fd33
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-02-12 11:31:30 +0800

    Started qrp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qrp::Application.config.secret_token = 'f6145272fc160b947d905003c32ee77d5101709e8b39f5ba0db39f398303cc198b03dcf5aa13327f91ab0f819b0f628deeb75bcba0a3d20d8edfef0c55736a29'
