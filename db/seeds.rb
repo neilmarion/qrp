a67c655391ca9295f875ae5763c7412026979a79
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-02-14 10:46:16 +0800

    Changes in the seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,6 @@ Purok.create({name: "PUROK 4 (BENZ COMPOUND)", to_lat: "14.4484055", to_lng: "12
 b = Marker.create({lat: "14.4908834", lng: "121.0551348", title: "TCU", html: "Taguig City University"})
 a = Marker.create({lat: "14.486479", lng: "121.0435477", title: "SM Bicutan", html: "SM Bicutan"})
 route = Route.create({from_marker_id: a.id, to_marker_id: b.id, title: "THIS IS KADIWA METRO MANILA SOUTH EDITION VENUE (23 FEB 2013)", html: '<img src="http://dl.dropbox.com/u/5938276/this_is_kadiwa_mms_ad_feb_23.jpg" width="100%">'})
-route.markers.create({lat: "14.4869361", lng: "121.0432473", title: "SM Bicutan", html: '<div>SM Bicutan</div> <img src="http://www.smprime.com/smprime/uploads/image/mallList/Bicutan.jpg" width="75%" height="75%">'})
+route.markers.create({lat: "14.4869361", lng: "121.0432473", title: "SM Bicutan", html: '<div>SM Bicutan</div> <img src="http://www.smprime.com/smprime/uploads/image/mallList/Bicutan.jpg" width="75%">'})
 
 
